dc4c3548ab35e87ec553a23c294cb465f14dd6a2
Łukasz Kęska <lukasz.keska@gmail.com>
2020-11-15 14:51:02 +0100

    etap: pipes, bez operatora "?"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-todo-list';
+  title = 'Moja lista zadań';
+
+  get footer():string{
+    return '© Lista zadań,All rights reserved.';
+  }
+
+  getDate():Date{
+    return new Date();
+  }
+
+  constructor(@Inject(LOCALE_ID) private localeId:string){
+    console.log(localeId);
+  }
 }
